fd60a43f90c23f12f08e3c12005eb82ade77f505
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-12-09 20:56:09 -0800

    Prep for help desk

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,12 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
+var currentVideoReducer = (state, {type, payload}) => {
   //TODO: define a reducer for the currentVideo field of our state.
-};
-
+  switch (type) {
+    case 'CURRENT_VIDEO':
+      return { 'CURRENT_VIDEO' : payload }
+    default:
+      return state;
+  };
+}
 export default currentVideoReducer;
